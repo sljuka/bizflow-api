ae2c5c46710ebd1e7a10774b6ee56b1848d3f90d
David Sljukic <davidsljukic@gmail.com>
2015-05-20 12:00:16 +0200

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"] || '3eb6db5a9026c547c72708438d496d942e976b252138db7e4e0ee5edd7539457d3ed0fa02ee5e7179420ce5290462018591adaf5f42adcf855da04877827def2'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
