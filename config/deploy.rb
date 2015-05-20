19cb9eee2880ba2b2ed603b524e50b2332ba8ebc
david sljukic <david.sljukic@gmail.com>
2015-05-20 16:43:41 +0200

    dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'bizflow_db')
 
-set :bundle_bins, fetch(:bundle_bins, []).push('bizflow')
+set :bundle_bins, fetch(:bundle_bins, []).push('bizflow, bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
