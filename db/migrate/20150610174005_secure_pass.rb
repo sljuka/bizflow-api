9ddfe0966a32a97675745e9eb1ecb3f5d4adc120
David Sljukic <davidsljukic@gmail.com>
2015-06-11 12:49:00 +0200

    mig

--- db/migrate/20150610174005_secure_pass.rb
+++ db/migrate/20150610174005_secure_pass.rb
@@ -1,6 +1,6 @@
 class SecurePass < ActiveRecord::Migration
   def change
     # add_column :users, :password_digest, :string
-    remove_column :users, :password, :string
+    # remove_column :users, :password, :string
   end
 end
